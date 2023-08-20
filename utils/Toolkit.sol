c227583372f88202660548f229d6c2d595e44977
0xm314 <0xm314@gmail.com>
2023-08-20 13:26:04 +0200

    updated toolkit

--- utils/Toolkit.sol
+++ utils/Toolkit.sol
@@ -1,23 +1,93 @@
 pragma solidity ^0.8.19;
-import { Math, Casting } from "prb-math/UD60x18.sol";
 
  contract Toolkit {
-    uint256 constant Q96 = 0x1000000000000000000000000;
+    uint256 constant PRECISION = 2**96;
 
-   /**
-     * @param token0Token1 the price of token0/token1 in token1 decimals
-     * @param token0Decimals the decimals of token0
-     * @return sqrtPriceX96 sqrt price of token0/token1
-     */
-    function encodePriceSqrt(uint256 token0Token1, uint8 token0Decimals)
-        pure
+    function sqrt(uint256 x) internal pure returns (uint256 z) {
+        assembly {
+            let y := x // We start y at x, which will help us make our initial estimate.
+
+            z := 181 // The "correct" value is 1, but this saves a multiplication later.
+
+            // This segment is to get a reasonable initial estimate for the Babylonian method. With a bad
+            // start, the correct # of bits increases ~linearly each iteration instead of ~quadratically.
+
+            // We check y >= 2^(k + 8) but shift right by k bits
+            // each branch to ensure that if x >= 256, then y >= 256.
+            if iszero(lt(y, 0x10000000000000000000000000000000000)) {
+                y := shr(128, y)
+                z := shl(64, z)
+            }
+            if iszero(lt(y, 0x1000000000000000000)) {
+                y := shr(64, y)
+                z := shl(32, z)
+            }
+            if iszero(lt(y, 0x10000000000)) {
+                y := shr(32, y)
+                z := shl(16, z)
+            }
+            if iszero(lt(y, 0x1000000)) {
+                y := shr(16, y)
+                z := shl(8, z)
+            }
+
+            // Goal was to get z*z*y within a small factor of x. More iterations could
+            // get y in a tighter range. Currently, we will have y in [256, 256*2^16).
+            // We ensured y >= 256 so that the relative difference between y and y+1 is small.
+            // That's not possible if x < 256 but we can just verify those cases exhaustively.
+
+            // Now, z*z*y <= x < z*z*(y+1), and y <= 2^(16+8), and either y >= 256, or x < 256.
+            // Correctness can be checked exhaustively for x < 256, so we assume y >= 256.
+            // Then z*sqrt(y) is within sqrt(257)/sqrt(256) of sqrt(x), or about 20bps.
+
+            // For s in the range [1/256, 256], the estimate f(s) = (181/1024) * (s+1) is in the range
+            // (1/2.84 * sqrt(s), 2.84 * sqrt(s)), with largest error when s = 1 and when s = 256 or 1/256.
+
+            // Since y is in [256, 256*2^16), let a = y/65536, so that a is in [1/256, 256). Then we can estimate
+            // sqrt(y) using sqrt(65536) * 181/1024 * (a + 1) = 181/4 * (y + 65536)/65536 = 181 * (y + 65536)/2^18.
+
+            // There is no overflow risk here since y < 2^136 after the first branch above.
+            z := shr(18, mul(z, add(y, 65536))) // A mul() is saved from starting z at 181.
+
+            // Given the worst case multiplicative error of 2.84 above, 7 iterations should be enough.
+            z := shr(1, add(z, div(x, z)))
+            z := shr(1, add(z, div(x, z)))
+            z := shr(1, add(z, div(x, z)))
+            z := shr(1, add(z, div(x, z)))
+            z := shr(1, add(z, div(x, z)))
+            z := shr(1, add(z, div(x, z)))
+            z := shr(1, add(z, div(x, z)))
+
+            // If x+1 is a perfect square, the Babylonian method cycles between
+            // floor(sqrt(x)) and ceil(sqrt(x)). This statement ensures we return floor.
+            // See: https://en.wikipedia.org/wiki/Integer_square_root#Using_only_integer_division
+            // Since the ceil is rare, we save gas on the assignment and repeat division in the rare case.
+            // If you don't care whether the floor or ceil square root is returned, you can remove this statement.
+            z := sub(z, lt(div(x, z), z))
+        }
+    }
+
+    /// @notice Encodes 1 token0 = X token1
+    function encodePriceSqrt(uint256 reserve1, uint256 reserve0)
         public
-        returns (uint160 sqrtPriceX96)
+        pure
+        returns (uint160)
     {
-        uint256 oneDecimals = 10 ** token0Decimals;
-        // ValueType.UD60x18 reserves = Casting.ud60x18(token0Token1 / uint256(oneDecimals));
-        uint256 sqrtReserves = Math.sqrt(Casting.ud60x18(token0Token1 / uint256(oneDecimals)));
-        uint256 sqrtPrice96 = sqrtReserves * Q96;
-        return uint160(sqrtPrice96);
+        return uint160(sqrt((reserve1 * PRECISION * PRECISION) / reserve0));
+    }
+
+    /// @notice Encodes R0 A0 = R1 A1, e.g. encodePriceSqrt(WETH, DAI, 1, 1000)
+    ///         Would mean 1 WETH = 1000 DAI
+    function encodePriceSqrt(
+        address a1,
+        address a0,
+        uint256 r1,
+        uint256 r0
+    ) public pure returns (uint160) {
+        if (a1 > a0) {
+            return encodePriceSqrt(r1, r0);
+        }
+
+        return encodePriceSqrt(r0, r1);
     }
  }
